db64a1f415901b55a6c953e88904b695d9eeb4da
Łukasz Sochacki <sochacki.luki@gmail.com>
2026-03-19 08:53:56 +0100

    Add Register component

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
-export const API_URL = 'http://localhost:9000';
+// export const API_URL = 'http://localhost:9000';
 export const IMGS_URL = 'http://localhost:9000/uploads';
 
-// export const API_URL =
-//   process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3131/api';
+export const API_URL =
+  process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:9000';
