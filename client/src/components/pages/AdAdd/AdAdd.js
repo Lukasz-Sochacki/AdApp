dac6559ddf22abfc89a5cb719d461f1e47a17f04
Łukasz Sochacki <sochacki.luki@gmail.com>
2026-03-20 16:23:13 +0100

    Ad AdAdd component

--- client/src/components/pages/AdAdd/AdAdd.js
+++ client/src/components/pages/AdAdd/AdAdd.js
@@ -1,5 +1,29 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, Navigate } from 'react-router-dom';
+import { addAdRequest } from '../../../redux/adsRedux';
+import { getUser } from '../../../redux/usersRedux';
+import AdForm from '../../features/AdForm/AdForm';
+
 const AdAdd = () => {
-  return <>AdAdd</>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector(getUser);
+
+  const handleSubmit = (fd) => {
+    dispatch(addAdRequest(fd)).then(() => {
+      navigate('/');
+    });
+  };
+
+  //block for not logged in users
+  if (!user) return <Navigate to='/' />;
+
+  return (
+    <div>
+      <h1 className='text-center my-4'>Add New Advertisement</h1>
+      <AdForm action={handleSubmit} actionText='Ad add' />
+    </div>
+  );
 };
 
 export default AdAdd;
