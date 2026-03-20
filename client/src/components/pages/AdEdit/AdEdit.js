f3b99493c2e2b79358d5384e8d3b9641a3b9eed5
Łukasz Sochacki <sochacki.luki@gmail.com>
2026-03-20 16:30:59 +0100

    Ad AdEdit component

--- client/src/components/pages/AdEdit/AdEdit.js
+++ client/src/components/pages/AdEdit/AdEdit.js
@@ -1,5 +1,42 @@
+import { useParams, Navigate, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAdById, editAdRequest } from '../../../redux/adsRedux';
+import { getUser } from '../../../redux/usersRedux';
+import AdForm from '../../features/AdForm/AdForm';
+
 const AdEdit = () => {
-  return <>AdEdit</>;
+  const { id } = useParams();
+  const ad = useSelector((state) => getAdById(state, id));
+  const user = useSelector(getUser);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  //if there is no ad in state (for example: refresh the page)
+  if (!ad) return <Navigate to='/' />;
+
+  //if user is not logged in or if user is not an author
+  if (!user || user.id !== ad.author._id) return <Navigate to='/' />;
+
+  const handleSubmit = (fd) => {
+    dispatch(editAdRequest(fd, id)).then(() => {
+      navigate(`/ad/${id}`);
+    });
+  };
+
+  return (
+    <div style={{ width: '70%', margin: 'auto' }}>
+      <h1 className='text-center my-4'>Edit Advertisement</h1>
+      <AdForm
+        action={handleSubmit}
+        actionText='Update Ad'
+        title={ad.title}
+        content={ad.content}
+        price={ad.price}
+        location={ad.location}
+      />
+    </div>
+  );
 };
 
 export default AdEdit;
