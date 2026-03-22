2c407ced63a3b385704f606c22886b759496ca23
Łukasz Sochacki <sochacki.luki@gmail.com>
2026-03-22 11:18:15 +0100

    Complete frontend part

--- client/src/redux/initialState.js
+++ client/src/redux/initialState.js
@@ -1,11 +1,6 @@
 const initialState = {
   ads: [],
   user: null,
-  // user: {
-  //   id: '69b7cc47059bab2bda2d8a1e',
-  //   login: 'Lukasz1234',
-  //   avatar: 'avatar.jpg',
-  // },
 };
 
 export default initialState;
