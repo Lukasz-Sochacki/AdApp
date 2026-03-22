bd51c586d5bc5ce764eb0f2296b0e8dbec6bb580
Łukasz Sochacki <sochacki.luki@gmail.com>
2026-03-22 11:36:40 +0100

    Fix server URLS

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 export const IMGS_URL =
   process.env.NODE_ENV === 'production'
     ? '/uploads/'
-    : 'http://localhost:9000/uploads/';
+    : 'http://localhost:9000/uploads';
 
 export const API_URL =
-  process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:9000';
+  process.env.NODE_ENV === 'production' ? '' : 'http://localhost:9000';
